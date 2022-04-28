8d08ef3c361a7fb884135dd5f281870ab7e68eae
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2022-04-28 05:48:59 +0200

    Add files via upload

---
+++ assets/slides.js
@@ -0,0 +1,23 @@
+"use strict";
+var slide = document.getElementById("slide");
+var upArrow = document.getElementById("upArrow");
+var downArrow = document.getElementById("downArrow");
+
+let x = 0;
+let width = document.querySelector(".exam-box").offsetWidth;
+let equation = width * 19;
+console.log(width);
+upArrow.addEventListener("click", (e) =>{
+    if(x > -equation){
+        x = x - width;
+    slide.style.left = x + "px"
+    };
+    e.preventDefault();
+});
+downArrow.addEventListener("click", (e) =>{
+    if(x < "0"){
+        x = x + width;
+    slide.style.left = x + "px"
+    };
+    e.preventDefault();
+});
