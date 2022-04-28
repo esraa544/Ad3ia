052ca4b5d2f0cbc285a523909ee962dd5c323571
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2022-04-29 01:25:47 +0200

    Update slides.js

--- assets/slides.js
+++ assets/slides.js
@@ -5,7 +5,7 @@ var downArrow = document.getElementById("upArrow");
 
 let x = 0;
 let width = document.querySelector(".exam-box").offsetWidth;
-let equation = width * 20;
+let equation = width * 21;
 console.log(width);
 upArrow.addEventListener("click", (e) =>{
     if(x > -equation){
